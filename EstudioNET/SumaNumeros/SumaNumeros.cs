51b8fe6d1ebdf0f544629e74a1c455922b8c92b6
Miguel Ángel <108587986+miguelangelgmm@users.noreply.github.com>
2023-10-20 00:03:50 +0200

    MAGMM: Suma de numeros

---
+++ EstudioNET/SumaNumeros/SumaNumeros.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumaNumeros
+{
+    internal class SumaNumeros
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca un número");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca el segundo número");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("La suma de los números es:" + (num1 + num2));
+
+            // Esperar a que el usuario presione una tecla antes de salir
+            Console.ReadKey();
+
+        }
+    }
+}
